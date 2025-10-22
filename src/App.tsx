2967fad3e21e16e099d95613aeed567966c91f3a
Luis Aguilar <luis.emmanuel.aguilar@gmail.com>
2025-10-22 13:12:25 -0600

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+
+function App() {
+
+  return (
+    <>
+      <div className="container">
+        <h1 className="app-title">
+          Cotizador de <span>Criptmonedas</span>
+        </h1>
+
+        <div className="content">
+
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
