ea221cd19817ac1f24ce24660120a9f343c6ccb2
Luis Aguilar <luis.emmanuel.aguilar@gmail.com>
2025-10-23 12:32:53 -0600

    Showing result in UI

--- src/store.ts
+++ src/store.ts
@@ -1,16 +1,18 @@
 import { create } from 'zustand'
 import { devtools } from 'zustand/middleware';
-import type { CryptoCurrency, Pair } from './types';
+import type { CryptoCurrency, CryptoPrice, Pair } from './types';
 import { getCryptos, fetchCurrentCryptoPrice } from './services/CryptoService';
 
 type CryptoStore = {
     cryptocurrencies: CryptoCurrency[];
+    result: CryptoPrice
     fetchCryptos: () => Promise<void>
     fetchData: (pair: Pair) => Promise<void>
 }
 
 export const useCryptoStore = create<CryptoStore>()(devtools((set) => ({
     cryptocurrencies: [],
+    result: {} as CryptoPrice,
     fetchCryptos: async () => {
         const cryptocurrencies = await getCryptos()
         set(() => ({
@@ -18,6 +20,9 @@ export const useCryptoStore = create<CryptoStore>()(devtools((set) => ({
         }))
     },
     fetchData: async (pair) => {
-        await fetchCurrentCryptoPrice(pair)
+        const result = await fetchCurrentCryptoPrice(pair)
+        set(() => ({
+            result,
+        }))
     }
 })))
