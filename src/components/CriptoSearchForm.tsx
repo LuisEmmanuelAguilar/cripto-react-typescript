330d4409017ddb869708b12ab9b8e6b0f42a3109
Luis Aguilar <luis.emmanuel.aguilar@gmail.com>
2025-10-22 23:18:16 -0600

    Showing data for UI

--- src/components/CriptoSearchForm.tsx
+++ src/components/CriptoSearchForm.tsx
@@ -1,6 +1,9 @@
+import { useCryptoStore } from "../store"
 import { currencies } from "../data"
 
 export default function CriptoSearchForm() {
+    const cryptocurrencies = useCryptoStore((state) => state.cryptocurrencies)
+
     return (
         <form className="form">
             <div className="field">
@@ -21,9 +24,14 @@ export default function CriptoSearchForm() {
                     name="criptocurrency"
                     id="criptocurrency">
                         <option value="">-- Seleccione --</option>
+                        { cryptocurrencies.map( crypto => (
+                            <option
+                                key={crypto.CoinInfo.FullName}
+                                value={crypto.CoinInfo.Name}
+                            >{crypto.CoinInfo.FullName}</option>
+                        ))}
                     </select>
             </div>
-
             <input type="submit" value="Cotizar" />
         </form>
     )
