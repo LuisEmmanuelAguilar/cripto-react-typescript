bb598a3d4ac6c0810a3e42359345bc02eb7f6a63
Luis Aguilar <luis.emmanuel.aguilar@gmail.com>
2025-10-22 23:17:39 -0600

    Adding single and multiple response schema

--- src/schema/crypto-schema.ts
+++ src/schema/crypto-schema.ts
@@ -4,3 +4,12 @@ export const CurrencySchema = z.object({
     code: z.string().min(3).max(3),
     name: z.string().min(1),
 })
+
+export const CryptoCurrencyResponseSchema = z.object({
+        CoinInfo : z.object({
+            Name: z.string(),
+            FullName: z.string(),
+        }),
+})
+
+export const CryptoCurrenciesResponseSchema = z.array(CryptoCurrencyResponseSchema)
