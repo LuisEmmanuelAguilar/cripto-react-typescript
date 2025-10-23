f95a4187164493d0be4587a54eb7f87445bb2b9e
Luis Aguilar <luis.emmanuel.aguilar@gmail.com>
2025-10-23 12:43:09 -0600

    Adding css rules

--- src/components/CryptoPriceDisplay.tsx
+++ src/components/CryptoPriceDisplay.tsx
@@ -8,13 +8,21 @@ export default function CryptoPriceDisplay() {
             && !Object.values(result).includes('')), [result])
     
     return (
-        <div>
+        <div className="result-wrapper">
             {hasResult && (
                 <>
                     <h2>Cotizacion</h2>
                     <div className="result">
+                        <img 
+                            src={`https://cryptocompare.com/${result.IMAGEURL}`} 
+                            alt="Crypto Image" 
+                        />
                         <div>
                             <p>El precio es de: <span>{result.PRICE}</span></p>
+                            <p>El precio mas alto del dia: <span>{result.HIGHDAY}</span></p>
+                            <p>El precio mas bajo del dia: <span>{result.LOWDAY}</span></p>
+                            <p>Variacion ultimas 24 horas: <span>{result.CHANGEPCT24HOUR}</span></p>
+                            <p>Ultima actualizacion: <span>{result.LASTUPDATE}</span></p>
                         </div>
                     </div>
                 </>
